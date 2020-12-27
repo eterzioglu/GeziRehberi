475c14ee4b1e6eaad1483edb3a440ff96ae4ed05
eterzioglu <eterzioglu8@gmail.com>
2020-12-28 00:26:32 +0300

    -

--- navigation/TabNavigator.js
+++ navigation/TabNavigator.js
@@ -1,33 +1,40 @@
-import React from 'react';
-import {createAppContainer} from 'react-navigation';
-import {createBottomTabNavigator} from 'react-navigation-tabs';
+import React from "react";
+import { createAppContainer } from "react-navigation";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import { AntDesign, Ionicons } from '@expo/vector-icons';
 
-import Flowers from '../screens/Flowers';
-import ProfilePage from '../screens/Profile';
+import Flowers from "../screens/Flowers";
+import ProfilePage from "../screens/Profile";
 
 class FlowerScreen extends React.Component {
   render() {
-    return (
-      <Flowers
-        navigator={navigator}
-      />
-    );
+    return <Flowers navigator={navigator} />;
   }
 }
 
 class ProfileScreen extends React.Component {
   render() {
-    return (
-      <ProfilePage
-        navigator={navigator}
-      />
-    );
+    return <ProfilePage navigator={navigator} />;
   }
 }
 
 const TabNavigator = createBottomTabNavigator({
-  Flowers: {screen: FlowerScreen},
-  Profile: {screen: ProfileScreen},
+  Flowers: {
+    screen: FlowerScreen,
+    navigationOptions: {
+      tabBarLabel: "Flowers",
+      tabBarIcon: ({ tintColor }) => (
+        <Ionicons name="ios-flower" color={tintColor} size={25} />
+      ),
+    },
+  },
+  Profile: {
+    screen: ProfileScreen,
+    navigationOptions: {
+      tabBarLabel: "Profile",
+      tabBarIcon: ({ tintColor }) => (
+        <AntDesign name="user" color={tintColor} size={25} />
+      ),
+    },
+  },
 });
-
-export default createAppContainer(TabNavigator);
